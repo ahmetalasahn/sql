0d1d016c58769b3de4a560acf45dc810f61e25b1
ahmme <ahmme@DESKTOP-3FL38SJ>
2021-01-27 23:50:30 +0300

    SQL

--- kurs5/Program.cs
+++ kurs5/Program.cs
@@ -1,12 +1,24 @@
-﻿using System;
+﻿--Select
+Select ContactName Adi, CompanyName SirketAdi, City Sehir From Customers
 
-namespace kurs5
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+Select * From Customers where City = 'Berlin'
+
+Select * From Products where CategoryID=1 and SupplierID=1
+
+Select * From Products order by CategoryID --asc=ascending,-desc=descending
+
+Select count(*) From Products
+
+Select CategoryID, count(*) From Products group by CategoryID having count(*)< 10
+
+Select Products.ProductID, Products.ProductName, Products.UnitPrice, Categories.CategoryName
+From Products inner join Categories on Products.CategoryID= Categories.CategoryID
+where Products.UnitPrice>25
+--DTA (DATA TRANSFORMATİON OBJECT)
+
+Select* From Products p left join [Order Details] od
+on p.ProductID = od.ProductID
+
+Select * From Customers c left join Orders o
+on c.CustomerID=o.CustomerID
+where o.CustomerID is null
